d645669ee01adb0e4717b6d478aa65e2d412d1ed
sidhant79 <108347450+sidhant79@users.noreply.github.com>
2023-08-13 12:42:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+
+video = cv2.VideoCapture("C:/WhiteHat jr/Classwork/class 119/c119-ObjectTracking-1-4-main/c119-ObjectTracking-1-4-main/bb3.mp4")
+
+#Load tracker
+tracker=cv2.TrackerCSRT_create()
+
+#Read the frame
+ret,img=video.read()
+
+#Select the bounding box on the image
+bbox=cv2.selectROI("Tracking",img,False)
+
+tracker.init(img,bbox)
+
+print(bbox)
+
+def drawBox(img,bbox):
+    x,y,w,h=int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(img,(x,y),(x+w,h+y),(255,0,255),3,1)
+    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+
+while True:
+    check,img=video.read()
+    success,bbox=tracker.update(img)
+
+    if success:
+        drawBox(img)
+    else:
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7(0,0,255),2)
+
+        key=cv2.waitKey(100)
+        if key==32:
+            print("Stopped")
+            break
+    video.release() 
+    cv2.destroyAllWindows()
